68d639c8a6efdf7fa7a36b9d1dc26b4b9b6f8925
Nikita Ermakov <Nikita_Ermakov@epam.com>
2019-09-20 17:55:05 +0300

    breaking build to check jenkins mail reporting

--- src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
@@ -30,7 +30,7 @@ import javax.persistence.MappedSuperclass;
 public class NamedEntity extends BaseEntity {
 
     @Column(name = "name")
-    private String name;
+    private String na\me;
 
     public String getName() {
         return this.name;
